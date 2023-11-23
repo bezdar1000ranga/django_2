f3ba286952a1235501d31a1fcc863ef146185ae9
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-24 05:47:02 +0700

    Маршруты для удаления, создания категорий

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,14 +1,13 @@
-from django.conf import settings
 from django.contrib.auth.views import LogoutView
-from django.template.context_processors import static
-
 from . import views
 from .views import user_login, create_design_request, view_own_requests, delete_design_request, change_status, \
-    add_category, manage_categories
+    add_category, manage_categories, user_profile, edit_design_request, delete_category
 from .views import register
 from .views import user_logout
 from .views import home
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home),
@@ -18,6 +17,16 @@ urlpatterns = [
     path('home/', home, name='home'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create_design_request/', create_design_request, name='create_design_request'),
+    path('delete_design_request/<int:request_id>/', delete_design_request, name='delete_design_request'),
+    path('change_status/<int:request_id>/<str:new_status>/', change_status, name='change_status'),
+    path('manage_categories/', manage_categories, name='manage_categories'),
+    path('add_category/', add_category, name='add_category'),
     path('view_own_requests/', view_own_requests, name='view_own_requests'),
-    path('view_own_requests/', view_own_requests, name='view_own_requests'),
+    path('user-profile/', user_profile, name='user_profile'),
+    path('', home, name='home'),
+    path('user-profile/', user_profile, name='user_profile'),
+    path('edit-design-request/<int:request_id>/',
+         edit_design_request, name='edit_design_request'),
+    path('delete_category/<int:category_id>/', delete_category, name='delete_category'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
