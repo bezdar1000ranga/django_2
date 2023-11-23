e0835abc283e08678e6a2a4fffaed31e242f900d
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-24 04:30:32 +0700

    админ панель

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CustomUser
+from .models import DesignRequest
 
-# Register your models here.
+admin.site.register(DesignRequest)
+admin.site.register(CustomUser)
