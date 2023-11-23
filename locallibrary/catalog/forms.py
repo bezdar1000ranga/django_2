c768ecc44c08b15dc89bf8b2d9d9ce737ac58549
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-24 04:30:21 +0700

    Формы для регистрации и входа

---
+++ locallibrary/catalog/forms.py
@@ -0,0 +1,33 @@
+from .models import CustomUser
+from .models import Category
+from django import forms
+from .models import DesignRequest
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ['fio', 'email', 'username', 'password', 'confirm_password', 'agreement']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError('Пароли не совпадают.')
+
+
+class DesignRequestForm(forms.ModelForm):
+    class Meta:
+        model = DesignRequest
+        fields = ['title', 'category', 'photo']
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
