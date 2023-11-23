7d3d0763cdf72e968a26f453ce4b5f30e7ad188f
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-24 04:30:01 +0700

    Подправил настройки

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'locallibrary', 'catalog', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'catalog', 'templates')],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
