b3186266c2ad2e93cf27444b420a5bdc38f9023d
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-24 03:58:13 +0700

    переделал настройки и маршруты

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for design project.
+"""locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,13 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('superadmin/', admin.site.urls),
-    path('user/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/applications/', permanent=True)),
-]
+    path('admin/', admin.site.urls),
+    path('catalog/',include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
