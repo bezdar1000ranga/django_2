59c56faf5344fc1ea177c5c840c60bb780928f78
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-24 04:47:57 +0700

    Модели дизайн-запросов и категории

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.models import BaseUserManager, User
 from django.contrib.auth.validators import ASCIIUsernameValidator
-from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator, FileExtensionValidator
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from django.conf import settings
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, help_text='Введите новую категорию заявки')
+
+    def __str__(self):
+        return self.name
 
 
 class CustomUserManager(BaseUserManager):
@@ -68,9 +76,10 @@ class CustomUser(AbstractUser):
 
 class DesignRequest(models.Model):
     title = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to='design_photos/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='design_request_photos/', blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
     STATUS_CHOICES = [
         ('New', 'Новая'),
         ('In Progress', 'Принято в работу'),
@@ -78,13 +87,25 @@ class DesignRequest(models.Model):
     ]
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='New')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image_admin = models.ImageField(verbose_name="Фотография дизайна", upload_to='images_admin/',
+                                    validators=[
+                                        FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg', 'bmp'])],
+                                    help_text="Указать фото дизайна при смене статуса на 'Выполнено'", blank=True)
+    comment_admin = models.TextField(verbose_name="Комментарий",
+                                     help_text="Указать комментарий при смене статуса на 'Принята в работу'",
+                                     blank=True)
+
+    def save(self, *args, **kwargs):
+        # Проверяем, изменился ли статус на "Выполнено" и есть ли изображение
+        if self.status == 'Выполнено' and not self.design_image:
+            raise ValidationError("При смене статуса на 'Выполнено' необходимо добавить изображение дизайна.")
+        super().save(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        # Проверяем, изменился ли статус на "Принято в работу" и есть ли комментарий
+        if self.status == 'Принято в работу' and not self.comments:
+            raise ValidationError("При смене статуса на 'Принято в работу' необходимо указать комментарий.")
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
